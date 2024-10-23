31416d63ae60ed37373202cddfc6cdaa9fa95f82
k201751 <k201751@nu.edu.pk>
2024-10-23 18:59:23 +0500

    credentialsCheck

--- js/script.js
+++ js/script.js
@@ -10,3 +10,42 @@ function loadLogin(){
 function loadSignup(){
     window.location.href = "./signup.html";
 }
+function checkSignupCredentials(){
+    let username = document.getElementsByName('username')[0]; 
+    let password = document.getElementsByName('password')[0];
+    let confirmPassword = document.getElementsByName('rpassword')[0];
+    let fname = document.getElementsByName('fname')[0]; 
+    let email = document.getElementsByName('email')[0];  
+    let errorArr = [];
+    let errorMsg = document.getElementById('error');
+
+    let usernameRegex = /^[a-zA-Z0-9_.]+$/;
+    let emailRegex = /^[a-zA-Z0-9_.]+@[a-z0-9]+.[a-z]+$/;
+    let fnameRegex = /^[a-zA-Z ]+$/;
+
+    if (!fnameRegex.test(fname.value)) {
+        errorArr.push('Name contain invalid characters, only letters are allowed.');
+    }
+    if (!emailRegex.test(email.value)) {
+        errorArr.push('Invalid Email!');
+    }
+    if (!usernameRegex.test(username.value)) {
+        errorArr.push('Username contains invalid characters! Only letters, numbers, and underscores are allowed.');
+    }
+
+    if(password.value !== confirmPassword.value){
+        errorArr.push('Passwords do not match!');
+    }
+
+    if(password.value.length < 8){
+        errorArr.push('Password should atleast be 8 characters.')
+    }
+    
+    if(errorArr.length > 0){
+        errorMsg.innerHTML = errorArr.join('\n');
+         // Display errors if any
+    } else {
+        errorMsg.innerHTML = "";
+        alert("Passwords match!");
+    }
+}
